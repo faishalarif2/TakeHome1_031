6331cb440747bf9e3426538169c8b07fdfc17c77
faishalarif2 <faishalarifn02@gmail.com>
2022-10-30 18:08:02 +0700

    commit

---
+++ src/main/java/takehome1/takehome1/Takehome1Application.java
@@ -0,0 +1,13 @@
+package takehome1.takehome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Takehome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Takehome1Application.class, args);
+	}
+
+}
---
+++ src/test/java/takehome1/takehome1/Takehome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package takehome1.takehome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
