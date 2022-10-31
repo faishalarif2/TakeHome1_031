6e93bf0037a52cf6d078db5906e87b85b0f021ad
faishalarif2 <faishalarifn02@gmail.com>
2022-10-31 12:48:55 +0700

    index

---
+++ src/main/java/takehome1/takehome1/Index.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package takehome1.takehome1;
+
+/**
+ *
+ * @author NITRO
+ */
+public class Index {
+    private String name;
+    private String lokasi;
+    private String gambar;
+    public Index(){
+        super();
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getlokasi(){
+        return lokasi;
+    }
+    public void setlokasi(String lokasi){
+        this.lokasi = lokasi;
+    }
+    public String getgambar(){
+        return gambar;
+    }
+    public void setgambar(String gambar){
+        this.gambar = gambar;
+    }
+    
+}
